5a8eebc9a79c86e7a09cde7d5d1e03a76511f7eb
mnkillebr <killebrew.biz@gmail.com>
2024-12-23 10:50:22 -0500

    add video playback for exercises

--- app/(tabs)/(exercises)/[exerciseId].tsx
+++ app/(tabs)/(exercises)/[exerciseId].tsx
@@ -3,8 +3,10 @@ import { ThemedView } from "@/components/ThemedView";
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { Stack, useLocalSearchParams, router } from "expo-router";
-import { View, Text } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { IconButton } from "react-native-paper";
+import { useEvent } from 'expo';
+import { useVideoPlayer, VideoView } from 'expo-video';
 
 export default function ExerciseDetail() {
   const { exerciseId } = useLocalSearchParams();
@@ -18,7 +20,15 @@ export default function ExerciseDetail() {
     queryKey: ['exercise', exerciseId.toString()],
     queryFn: () => api.exercises.exerciseDetail(exerciseId.toString())
   });
-  console.log("exercise id", exerciseId, exercise)
+  // console.log("exercise id", exerciseId, exercise && Object.keys(exercise), exercise && exercise.videoToken)
+
+  const videoSource = exercise ? `https://stream.mux.com/${exercise.muxPlaybackId}.m3u8?token=${exercise.videoToken}` : ''
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = false;
+    // player.play();
+  });
+
+  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
 
   return (
     <>
@@ -34,9 +44,46 @@ export default function ExerciseDetail() {
           title: exercise?.name || 'Exercise Details'
         }}
       />
-      <ThemedView>
-        <ThemedText>Exercise Detail</ThemedText>
+      <ThemedView className="flex-1">
+        <ThemedView className='p-4'>
+          {/* <ThemedText>Exercise Detail</ThemedText> */}
+          {exercise ? (
+            <View style={styles.contentContainer}>
+              <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
+              <View style={styles.controlsContainer}>
+                <Button
+                  title={isPlaying ? 'Pause' : 'Play'}
+                  onPress={() => {
+                    if (isPlaying) {
+                      player.pause();
+                    } else {
+                      player.play();
+                    }
+                  }}
+                />
+              </View>
+            </View>
+          ) : null}
+        </ThemedView>
       </ThemedView>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    // padding: 10,
+    // marginTop: 100,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // paddingHorizontal: 50,
+  },
+  video: {
+    width: '100%',
+    aspectRatio: 9/16,
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+});
